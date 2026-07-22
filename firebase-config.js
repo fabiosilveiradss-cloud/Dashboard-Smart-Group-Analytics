9d687ef9426995587f52be297375982b8ee1a81b
fabiosilveiradss-cloud <fabiosilveiradss@gmail.com>
2026-07-22 13:42:29 -0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+//-----------------------------------------------------
+// CONFIGURAÇÃO DO FIREBASE
+//-----------------------------------------------------
+
+import { initializeApp } from
+    "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import { getAuth } from
+    "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIPyLxiDY_BNtxMtHUqXtEv09l-Cibxaw",
+    authDomain: "smart-group-analytics.firebaseapp.com",
+    projectId: "smart-group-analytics",
+    storageBucket: "smart-group-analytics.firebasestorage.app",
+    messagingSenderId: "463021033688",
+    appId: "1:463021033688:web:466d02021f8831cc4befad"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
