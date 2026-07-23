1a3a41825ce12eae08ef87c5951fc3a790b6a822
fabiosilveiradss-cloud <fabiosilveiradss@gmail.com>
2026-07-23 08:27:24 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,51 @@
 //-----------------------------------------------------
-// CONFIGURAÇÃO DO FIREBASE
+// IMPORTAÇÕES DO FIREBASE
 //-----------------------------------------------------
 
-import { initializeApp } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import { getAuth } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+//-----------------------------------------------------
+// CONFIGURAÇÃO DO PROJETO
+//-----------------------------------------------------
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIPyLxiDY_BNtxMtHUqXtEv09l-Cibxaw",
-    authDomain: "smart-group-analytics.firebaseapp.com",
-    projectId: "smart-group-analytics",
-    storageBucket: "smart-group-analytics.firebasestorage.app",
-    messagingSenderId: "463021033688",
-    appId: "1:463021033688:web:466d02021f8831cc4befad"
+  apiKey: "AIzaSyBIPyLxiDY_BNtxMtHUqXtEv09l-Cibxaw",
+  authDomain: "smart-group-analytics.firebaseapp.com",
+  projectId: "smart-group-analytics",
+  storageBucket: "smart-group-analytics.firebasestorage.app",
+  messagingSenderId: "463021033688",
+  appId: "1:463021033688:web:466d02021f8831cc4befad"
 };
+
+
+//-----------------------------------------------------
+// INICIALIZAÇÃO
+//-----------------------------------------------------
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export { app, auth };
+const db = getFirestore(app);
+
+
+//-----------------------------------------------------
+// EXPORTAÇÕES
+//-----------------------------------------------------
+
+export {
+  app,
+  auth,
+  db
+};
